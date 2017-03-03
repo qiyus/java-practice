65a38118dfb30eca4bb7429d43a06552a62558e6
qiyus <hsly_song@sina.com>
2017-03-03 18:04:34 +0800

    多线程练习2。

---
+++ src/main/java/point/thread/PrimeGenerator.java
@@ -0,0 +1,53 @@
+package point.thread;
+
+/**
+ * Created by Vigor on 2017/3/3.
+ * 多线程练习2
+ * 1，线程的中断。
+ */
+public class PrimeGenerator extends Thread{
+
+    @Override
+    public void run() {
+        long number = 1L;
+        while (true) {
+            if (isPrime(number)) {
+                System.out.println(number);
+            }
+
+            if (isInterrupted()) {
+                System.out.printf("The Prime Generator has been Interrupted");
+                return;
+            }
+
+            number++;
+        }
+    }
+
+    private boolean isPrime(long number) {
+        if (number <= 2) {
+            return true;
+        }
+
+        for (long i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Thread thread = new PrimeGenerator();
+        thread.start();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        thread.interrupt();
+    }
+}
